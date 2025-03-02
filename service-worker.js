e87fa2631957d2a2f723e682a3dcfad698e1a4ab
Nery <79871298+imperiousnery@users.noreply.github.com>
2025-03-02 13:59:12 -0400

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'beta';
+var cacheName = 'invisible-scars';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
